464775d62aaf5e2bf5cfa133f4ca73187c7bfb7c
Felipe Dias <felipedias.dev@gmail.com>
2023-04-26 23:48:04 -0300

    feat: :sparkles: add hooks pre and post Run

--- cmd/category.go
+++ cmd/category.go
@@ -20,11 +20,19 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Init Run")
 		// name, _ := cmd.Flags().GetString("name")
 		fmt.Println("category called with name:", category)
 		exists, _ := cmd.Flags().GetBool("exists")
 		fmt.Println("category exists:", exists)
+		fmt.Println("Finish Run")
 
+	},
+	PreRun: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Called before Run")
+	},
+	PostRun: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Called after Run")
 	},
 }
 
